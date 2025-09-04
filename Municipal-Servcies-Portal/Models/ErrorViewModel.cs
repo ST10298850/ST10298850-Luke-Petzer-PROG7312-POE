243c4009f4b018507b5afaa9b899321c8d19acb4
Luke Petzer <luke.petzer@icloud.com>
2025-09-04 18:52:16 +0200

    Initial commit - MVC project setup

---
+++ Municipal-Servcies-Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Municipal_Servcies_Portal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
