d567d2de34a4674caa042ce47981c3ca36397d47
Luke Petzer <luke.petzer@icloud.com>
2025-09-05 00:11:35 +0200

    changes made to the following files: - implemented the design for the create issue request - Changes made to the navigation to allow users to swap between the pages. - Changed file for styling, attempting to create global styling to maximise page efficiency

--- Municipal-Servcies-Portal/Controllers/IssueController.cs
+++ Municipal-Servcies-Portal/Controllers/IssueController.cs
@@ -1,6 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace Municipal_Servcies_Portal.Controllers;
 
-public class IssueController
+public class IssueController : Controller
 {
-    
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    public IActionResult Confirmation()
+    {
+        return View();
+    }
 }
